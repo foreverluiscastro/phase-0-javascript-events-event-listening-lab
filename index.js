dd3ad7644d8cc54d5a82ead2c7bf1cbaee2254fe
Luis <foreverluiscastro@gmail.com>
2022-05-31 13:39:24 -0400

    commit tree d81097ab8e03779be2c91c7bc08fec87693f2627

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+// function addingEventListener() {
+//     const input = document.getElementById('input');
+
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert); 
+// }
 function addingEventListener() {
-    const input = document.getElementById('input');
 
-function clickAlert() {
-  alert('I was clicked!');
-}
-
-input.addEventListener('click', clickAlert); 
 }
